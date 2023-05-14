700c51a3b1979514f5764ce413c06f1110be45e8
Koen Hufkens <koen.hufkens@gmail.com>
2023-05-14 19:36:45 +0200

    demo data download and test scripts

--- analysis/download_demo_data.R
+++ analysis/download_demo_data.R
@@ -4,6 +4,29 @@ library(appeears)
 user <- "khufkens"
 options(keyring_backend = "file")
 
+#---- download point data ----
+
+task_time_series <- data.frame(
+  task = "time_series",
+  subtask = "US-Ha1",
+  latitude = 42.5378,
+  longitude = -72.1715,
+  start = "2010-01-01",
+  end = "2010-12-31",
+  product = "MCD43A4.061",
+  layer = c("Nadir_Reflectance_Band3","Nadir_Reflectance_Band4")
+) |> appeears::rs_build_task()
+
+rs_request(
+  request = task_time_series,
+  user = "khufkens",
+  transfer = TRUE,
+  path = "inst/extdata/",
+  verbose = TRUE
+)
+
+#---- download area based data ----
+
 df <- data.frame(
   task = "time_series",
   subtask = "US-Ha1",
@@ -13,10 +36,6 @@ df <- data.frame(
   end = "2010-12-31",
   product = "MCD43A4.061",
   layer = c("Nadir_Reflectance_Band3","Nadir_Reflectance_Band4")
-)
-
-task <- rs_build_task(
-  df = df
 )
 
 # load the north carolina demo data
@@ -55,14 +74,6 @@ rs_request(
 
 rs_request(
   request = task_roi_2,
-  user = "khufkens",
-  transfer = TRUE,
-  path = "inst/extdata/",
-  verbose = TRUE
-)
-
-rs_request(
-  request = task,
   user = "khufkens",
   transfer = TRUE,
   path = "inst/extdata/",
