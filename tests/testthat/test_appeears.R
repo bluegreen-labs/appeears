bc6fb23f2545f091ab20f9656496c7723472aad4
Koen Hufkens <koen.hufkens@gmail.com>
2023-05-15 20:11:56 +0200

    unit tests + version bump

--- tests/testthat/test_appeears.R
+++ tests/testthat/test_appeears.R
@@ -1,5 +1,7 @@
-# set options
+#----- set options -----
 options(keyring_backend="file")
+library(sf)
+library(terra)
 
 # spoof keyring
 if(!("appeears" %in% keyring::keyring_list()$keyring)){
@@ -32,3 +34,54 @@ if(server_check & ON_GIT){
   # the user is not created
   login_check <- inherits(user, "try-error")
 }
+
+df <- data.frame(
+  task = "time_series",
+  subtask = "US-Ha1",
+  latitude = 42.5378,
+  longitude = -72.1715,
+  start = "2010-01-01",
+  end = "2010-12-31",
+  product = "MCD12Q2.006",
+  layer = "Greenup"
+)
+
+rs_request(
+  request = task_time_series,
+  user = "khufkens",
+  transfer = TRUE,
+  path = "inst/extdata/",
+  verbose = TRUE
+)
+
+#---- test functions ----
+
+test_that("test functions without logins", {
+  skip_on_cran()
+  skip_if(login_check)
+
+  # list products / layers
+  expect_true(inherits(rs_products(), "data.frame"))
+  expect_true(inherits(rs_layers("MCD12Q2.006"), "data.frame"))
+
+  # create tasks
+  expect_type(rs_build_task(df), "character")
+
+
+
+
+
+})
+
+
+test_that("test data transfers", {
+  skip_on_cran()
+  skip_if(login_check)
+
+  expect_true(inherits(rs_products(), "data.frame"))
+  expect_true(inherits(rs_layers("MCD12Q2.006"), "data.frame"))
+  expect_type(rs_build_task(df), "character")
+
+})
+
+
